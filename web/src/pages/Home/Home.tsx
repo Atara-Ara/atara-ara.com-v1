26de229dd20eb482f26a67f6cdf01e1e24941665
Chandler Chilvers <cwchilvers@gmail.com>
2024-03-13 14:42:05 -0700

    Updates to home page and listen page.

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,41 +1,31 @@
 import './Home.sass';
-import BackgroundImage from './assets/background.jpeg';
-import ContentImage from './assets/content.jpeg';
-import Star from './assets/Star.tsx';
-import Title from './assets/Title.tsx';
+import backgroundImage from './assets/background.jpeg';
+import headerImage from './assets/header.svg';
+import contentImage from './assets/content.jpeg'
 
 export const Home = () => {
     const background = {
         color: 'black',
-        image: `url(${BackgroundImage})`
+        image: `url(${backgroundImage})`
     }
 
     const content = {
         backgroundColor: 'black',
-        textColor: 'white',
-        type: 'New Single',
-        title: `EVERYTHING'S OKAY`,
-        message: 'Out Now',
-        image: `url(${ContentImage})`,
-        imageAlt: "Everything's Okay Single Cover",
+        header: headerImage,
+        headerAlt: "New Single, Everything's Okay. Out Now!",
+        image: contentImage,
+        imageAlt: "Artwork for the single, Everything's Okay.",
         link: 'https://ampl.ink/v06yY',
     };
 
     return (
-        <section id='home' className='home'>
-            <div className='background-image' style={{ backgroundColor: background.color, backgroundImage: background.image }}></div>
-            <div className='featured-content' style={{ color: content.textColor }}>
+        <section id='home' className='home' style={{ backgroundColor: background.color, backgroundImage: background.image }}>
+            <div className='featured-content'>
                 <div className='background' style={{ backgroundColor: content.backgroundColor }}></div>
-                <div className='header'>
-                    <div className='type'>
-                        <Star color="orange" />
-                        <h2>{content.type}</h2>
-                    </div>
-                    <div className='title'><Title color='white' /></div>
-                    <h2 className='message'>{content.message}</h2>
-                </div>
 
-                <img src={ContentImage} alt={content.imageAlt} className='image' />
+                <img className='header' src={content.header} alt={content.imageAlt} />
+                <img className='image' src={content.image} alt={content.imageAlt} />
+
 
                 <div className='cta'>
                     <a href={content.link} className=' button'>stream / purchase</a>
--- web/src/pages/Listen/Listen.tsx
+++ web/src/pages/Listen/Listen.tsx
@@ -1,9 +1,40 @@
 import './Listen.sass';
+import backgroundImage from './assets/background.jpeg';
+import oceanSkiesImage from './assets/occean-skies.jpeg';
+import everythingsOkayImage from './assets/everythings-okay.jpeg';
 
 export const Listen = () => {
+    const background = {
+        color: 'black',
+        image: `url(${backgroundImage})`
+    }
+
+    const releases = [
+        {
+            title: 'Ocean Skies',
+            image: oceanSkiesImage,
+            link: 'https://ampl.ink/7jD7y'
+        },
+        {
+            title: "Everything's Okay",
+            image: everythingsOkayImage,
+            link: "https://ampl.ink/v06yY"
+        }
+    ];
+
     return (
-        <section id='listen' className='listen'>
+        <section id='listen' className='listen' style={{ backgroundColor: background.color, backgroundImage: background.image }}>
             <h1>LISTEN</h1>
+            <div className='releases'>
+                <div className='release'>
+                    <img src={releases[0].image} alt={releases[0].title} />
+                    {/* <a href={releases[0].link} className='button'>stream / purchase</a> */}
+                </div>
+                <div className='release'>
+                    <img src={releases[1].image} alt={releases[1].title} />
+                    {/* <a href={releases[1].link} className='button'>stream / purchase</a> */}
+                </div>
+            </div>
         </section>
     )
 };
