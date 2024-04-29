9793f50667ee1ebfca41f1bceb9043b74233fab0
Chandler Chilvers <cwchilvers@gmail.com>
2024-04-29 15:32:48 -0700

    Fixed cursor bugs. Removed cursor for mobile devices.

--- web/src/components/Cursor/Cursor.tsx
+++ web/src/components/Cursor/Cursor.tsx
@@ -4,8 +4,12 @@ import { useState, useEffect } from 'react';
 export const Cursor = () => {
     const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
     const [isMouseDown, setIsMouseDown] = useState(false);
+    const [isMobileDevice, setIsMobileDevice] = useState(false);
 
     useEffect(() => {
+        const userAgent = navigator.userAgent.toLowerCase();
+        setIsMobileDevice(/android|webos|iphone|ipad|ipod|blackberry|windows phone/.test(userAgent));
+
         const updateCursorPosition = (e: MouseEvent) => {
             setCursorPosition({ x: e.clientX, y: e.clientY });
         };
@@ -30,6 +34,6 @@ export const Cursor = () => {
     }, []);
 
     return (
-        <div className={`cursor ${isMouseDown ? 'mouse-down' : ''}`} style={{ left: cursorPosition.x, top: cursorPosition.y }}></div>
+        <div className={`cursor ${isMouseDown ? 'mouse-down' : ''} ${isMobileDevice ? 'hide-on-mobile' : ''}`} style={{ left: cursorPosition.x, top: cursorPosition.y }}></div>
     );
 };
