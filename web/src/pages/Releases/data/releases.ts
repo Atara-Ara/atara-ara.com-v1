1040ca9a8e9c909cea22f2d6bc416822028071e8
Chandler Chilvers <cwchilvers@gmail.com>
2024-04-29 23:14:01 -0700

    Updates to home page, releases, and menu.

--- web/src/pages/Releases/data/releases.ts
+++ web/src/pages/Releases/data/releases.ts
@@ -8,21 +8,21 @@ const data = [
         title: "3AM",
         release_date: "2024-04-12",
         image: release1Image,
-        link: ""
+        link: "https://ffm.to/koqo4zo"
     },
     {
         id: "D99A6yRr",
         title: "Everything's Okay",
         release_date: "2024-03-08",
         image: release2Image,
-        link: "https://ampl.ink/v06yY"
+        link: "https://open.spotify.com/track/3Uc2j77MUHrtvhbFf3bcpD?si=672d8236be9b4cec"
     },
     {
         id: "3p0X57Ng",
         title: "Ocean Skies",
         release_date: "2023-07-21",
         image: release3Image,
-        link: ""
+        link: "https://open.spotify.com/track/7dIn5DzOgYrwBdQVoqh79o?si=318eb38644834127"
     }
 ];
 
