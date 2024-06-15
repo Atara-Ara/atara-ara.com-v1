28ae712a3ae13053c0c9dea5ac76d2dec7115ddb
Chandler Chilvers <cwchilvers@gmail.com>
2024-06-14 21:19:25 -0700

    Updated shows and home page for tour.

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 import './Home.sass';
 import { useState } from 'react';
-import data from './data/home.json';
+// import data from './data/home.json';
 import backgroundImage from './assets/background.jpeg';
-import headerImage from './assets/header.svg';
+// import headerImage from './assets/header.svg';
 import contentImage from './assets/content.jpeg';
 
 export const Home = () => {
     const [background] = useState({ color: 'black' });
+    {/* const [background] = useState(data.background[0]); 
     const [content] = useState({
         backgroundColor: data.content[0].backgroundColor,
         headerAlt: data.content[0].headerAlt,
@@ -21,6 +22,16 @@ export const Home = () => {
                 <img className='header' src={headerImage} alt={content.headerAlt} />
                 <img className='image' src={contentImage} alt={content.imageAlt} />
                 <a className='cta' href={content.link}>stream / purchase</a>
+            </div>
+        </section>
+    );
+    */}
+
+    return (
+        <section id='home' className='home' style={{ backgroundColor: background.color, backgroundImage: `url(${backgroundImage})` }}>
+            <div className='featured-content'>
+                <div className='background' style={{ backgroundColor: 'black' }}></div>
+                <img className='image' src={contentImage} alt='content' />
             </div>
         </section>
     );
--- web/src/pages/Shows/Shows.tsx
+++ web/src/pages/Shows/Shows.tsx
@@ -3,13 +3,15 @@ import { useState } from 'react';
 import backgroundImage from './assets/background.jpeg';
 import title from './assets/title.svg';
 import data from './data/shows.json';
+import LocationIcon from './assets/location.svg';
 
 interface Show {
     id: string;
     date: string;
     venue: string;
-    address: string;
-    time: string;
+    city: string;
+    tickets: string;
+    gmaps: string;
 }
 
 export const Shows = () => {
@@ -18,34 +20,48 @@ export const Shows = () => {
     return (
         <section id='shows' className='shows' style={{ backgroundImage: `url(${backgroundImage})` }}>
             <img className='title' src={title} alt='Shows' />
+            <h4>Select event for more details.</h4>
             <div className='container'>
                 {shows.length > 0 ? (
                     shows.map(show => (
-                        <div className='show' key={show.id}>
-                            <div className='left'>
-                                <div className='date-left'>
-                                    <p className='month'>{new Date(show.date).toLocaleString('default', { month: 'short' })}</p>
-                                    <p className='year'>{new Date(show.date).getFullYear()}</p>
+                        <a href={show.tickets} target='_blank' rel='noreferrer'>
+                            <div className='show' key={show.id}>
+                                <div className='left-container'>
+                                    <div className='left'>
+                                        <div className='date-left'>
+                                            <p className='month'>{new Date(show.date).toLocaleString('default', { month: 'short' })}</p>
+                                            <p className='year'>{new Date(show.date).getFullYear()}</p>
+                                        </div>
+                                        <div className='date-right'>
+                                            <p>{new Date(show.date).getDate()}</p>
+                                        </div>
+                                    </div>
+                                    <div className='info'>
+                                        <div className='row-1'>
+                                            <h1 className='venue'>{show.venue}</h1>
+                                            <a href={show.gmaps} target='_blank' rel='noreferrer'>
+                                                <img className='location-icon' src={LocationIcon} alt='Google Maps' />
+                                            </a>
+                                        </div>
+                                        <p className='city'>{show.city}</p>
+                                    </div>
                                 </div>
-                                <div className='date-right'>
-                                    <p>{new Date(show.date).getDate()}</p>
-                                </div>
+
+                                {/* 
+                                <a href={show.tickets} target='_blank' rel='noreferrer' className='tickets'>
+                                    <p>Tickets</p>
+                                </a> 
+                            */}
                             </div>
-                            <div className='info'>
-                                <h1 className='venue'>{show.venue}</h1>
-                                <p className='address'>{show.address}</p>
-                            </div>
-                            <div className='right'>
-                                <p>{show.time}</p>
-                            </div>
-                        </div>
+                        </a>
                     ))
+
                 ) : (
                     <div>
                         <p>No upcoming shows at the moment</p>
                     </div>
                 )}
             </div>
-        </section>
+        </section >
     );
 };
